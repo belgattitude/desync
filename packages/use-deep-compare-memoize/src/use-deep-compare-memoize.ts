ab17e7528f1b2b5aeafa7f56c08e87c5bc895d31
Sébastien Vanvelthem <belgattitude@gmail.com>
2020-04-25 10:47:58 +0200

    feat: bypass dequal if strict compare works

--- packages/use-deep-compare-memoize/src/use-deep-compare-memoize.ts
+++ packages/use-deep-compare-memoize/src/use-deep-compare-memoize.ts
@@ -1,11 +1,11 @@
 import deepEqual from 'dequal';
 import { useRef } from 'react';
 
-type AcceptedValue = string | boolean | object | null;
+type SupportedValue = string | boolean | object | null;
 
-export function useDeepCompareMemoize(value: AcceptedValue): AcceptedValue {
-  const ref = useRef<AcceptedValue>(null);
-  if (!deepEqual(value, ref.current)) {
+export function useDeepCompareMemoize(value: SupportedValue): SupportedValue {
+  const ref = useRef<SupportedValue>(null);
+  if (value !== ref && !deepEqual(value, ref.current)) {
     ref.current = value;
   }
   return ref.current;
