857371f2d364e4542da03fe073e5683c747e8710
Sébastien Vanvelthem <belgattitude@gmail.com>
2020-05-31 12:10:52 +0200

    feat: use Record instead of object

--- packages/use-deep-compare-memoize/src/use-deep-compare-memoize.ts
+++ packages/use-deep-compare-memoize/src/use-deep-compare-memoize.ts
@@ -1,11 +1,11 @@
 import { default as deepEqual } from 'dequal';
 import { useRef } from 'react';
 
-type SupportedValue = string | boolean | object | null;
+type SupportedValue = Record<string, unknown> | string | boolean | number | null;
 
 export function useDeepCompareMemoize(value: SupportedValue): SupportedValue {
   const ref = useRef<SupportedValue>(null);
-  if (value !== ref && !deepEqual(value, ref.current)) {
+  if (!deepEqual(value, ref.current)) {
     ref.current = value;
   }
   return ref.current;
