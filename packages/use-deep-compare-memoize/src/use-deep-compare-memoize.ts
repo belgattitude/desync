57fe545e0c59e70b1842d3c93b5d054b7c3d5e7a
Sébastien Vanvelthem <belgattitude@gmail.com>
2020-09-07 20:19:34 +0200

    feat(use-deep-compare-memoize): updated to dequal 2

--- packages/use-deep-compare-memoize/src/use-deep-compare-memoize.ts
+++ packages/use-deep-compare-memoize/src/use-deep-compare-memoize.ts
@@ -1,11 +1,11 @@
-import { default as deepEqual } from 'dequal';
+import { dequal } from 'dequal';
 import { useRef } from 'react';
 
 type SupportedValue = Record<string, unknown> | string | boolean | number | null;
 
 export function useDeepCompareMemoize(value: SupportedValue): SupportedValue {
   const ref = useRef<SupportedValue>(null);
-  if (!deepEqual(value, ref.current)) {
+  if (!dequal(value, ref.current)) {
     ref.current = value;
   }
   return ref.current;
